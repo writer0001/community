79182bc9d8db252d5ef873e522adde68f05dd8c5
writer <2844194920@qq.com>
2020-03-01 16:01:19 +0800

    extract github config from code to properties

--- src/main/java/com/gewenjie/study/demo/controller/AuthorizeController.java
+++ src/main/java/com/gewenjie/study/demo/controller/AuthorizeController.java
@@ -4,25 +4,36 @@ import com.gewenjie.study.demo.dto.AccessTokenDTO;
 import com.gewenjie.study.demo.dto.GithubUser;
 import com.gewenjie.study.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+    //通过自动注入的方式，得到了githubprovider对象，下面就可以调用它的发送post请求和get请求得到用户信息的方法了。
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+    //我们在这里接受了身份验证通过后传回来code和state用于后面请求access_token
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO =new AccessTokenDTO();    //下面这些都是为了发送post请求而设置的参数
-        accessTokenDTO.setClient_id("2d70f42f4349939b8818");
-        accessTokenDTO.setClient_secret("70ac4eb7e35ec0ac5db0a0041a6319c12b246c10");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        String access_token = githubProvider.getAccessToken(accessTokenDTO);            //调用服务发送post请求
-        GithubUser user = githubProvider.getUser(access_token);
+        String access_token = githubProvider.getAccessToken(accessTokenDTO);//调用服务发送post请求，得到access_token
+        GithubUser user = githubProvider.getUser(access_token);             //通过access_token得到用户信息
         System.out.println(user.getName());
         return "index";
     }
--- src/main/java/com/gewenjie/study/demo/provider/GithubProvider.java
+++ src/main/java/com/gewenjie/study/demo/provider/GithubProvider.java
@@ -18,34 +18,38 @@ springboot上下文就会自动创建对象然后将对象赋值给添加了@Aut
 而用@Component对那些比较中立的类进行凝视*/
 @Component
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){//把需要的参数穿进阿狸，然后就可以发送请求，并返回access_token
+        //创建一个mediatype，设置字符集和信息传输格式为json
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
-
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient();//创建一个客户端，我们一会儿就是用这个来发送请求（request）
+        //创建post的body，把mediatype和json格式的数据作为参数传入
         RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
-        Request request = new Request.Builder()
+        Request request = new Request.Builder()//创建一个请求，然后设置url和body，通过build方法建立起来
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
-        try (Response response = client.newCall(request).execute()) {
-            String string = response.body().string();
-            String token = string.split("&")[0].split("=")[1];
+        try (Response response = client.newCall(request).execute()) { //通过客户端创建一个新的会话将请求传入并执行，用response接受
+            String string = response.body().string();//接受返回的参数部分并付给一个字符串
+            String token = string.split("&")[0].split("=")[1];//通过分割方法得到需要的token。
             return token;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+    //通过accesstoken得到用户信息
     public GithubUser getUser(String accessToken){
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
+        OkHttpClient client = new OkHttpClient();           //创建一个客户端
+        Request request = new Request.Builder()             //创建一个请求，设置url
                 .url("https://api.github.com/user?access_token="+accessToken)
                 .build();
         try {
-            Response response = client.newCall(request).execute();
-            String string = response.body().string();
+            Response response = client.newCall(request).execute();//用客户端建立会话发送请求，并用response接受返回来的消息
+            String string = response.body().string();             //将返回来的消息转化成字符串格式
+            //使用fastjson包里的parseObject方法，将返回来的信息解析成对象，放入到githuuser当中去。这里要注意的是，githubUser这个
+            //dto中的属性名应该和json中的key名一样才行，没有创建的属性名，就不会赋值到githubUser当中
             GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
-            return githubUser;
+            return githubUser;//返回用户信息
         } catch (Exception e) {
             e.printStackTrace();
         }
